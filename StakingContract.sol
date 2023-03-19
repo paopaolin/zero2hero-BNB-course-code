a491b9f7d523fde20a8ef89758cde72bdbeaef59
paopaolin <88531334+paopaolin@users.noreply.github.com>
2023-03-20 07:28:21 +0800

    Update StakingContract.sol

--- StakingContract.sol
+++ StakingContract.sol
@@ -1,18 +1,14 @@
-// https://github.com/owner/repo/blob/branch/path/to/Contract.sol
-
-import "https://github.com/owner/repo/blob/branch/path/to/Contract.sol";
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
-import "./HodlerCoin.sol";
-import "./MGLToken.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ERCmgl.sol";
+import "./Sushi.sol";
 
 contract StakeToEarn is ReentrancyGuard, Context {
-    MGLToken stakingToken;
-    HodlerCoin rewardsToken;
+    Sushi stakingToken;
+    ERC20mgl rewardsToken;
 
     uint256 constant RAY = 10**27;
 
@@ -27,7 +23,56 @@ contract StakeToEarn is ReentrancyGuard, Context {
     uint256 public totalStakedAmount;
 
     constructor(address stakingTokenAddr, address rewardsTokenAddr, uint _rewardEverySecond) {
-        stakingToken = MGLToken(stakingTokenAddr);
-        rewardsToken = HodlerCoin(rewardsTokenAddr);
+        stakingToken = Sushi(stakingTokenAddr);
+        rewardsToken = ERC20mgl(rewardsTokenAddr);
         rewardEverySecond = _rewardEverySecond;
     }
+
+    function stake(uint256 amount) external nonReentrant {
+        _updateIndex();
+
+        stakingToken.transferFrom(_msgSender(), address(this), amount);
+        userStakedAmount[_msgSender()] += amount;
+        totalStakedAmount += amount;
+    }
+
+    function withdraw(uint256 amount) external nonReentrant {
+        _updateIndex();
+
+        stakingToken.transfer(_msgSender(), amount);
+        userStakedAmount[_msgSender()] -= amount;
+        totalStakedAmount -= amount;
+    }
+
+    function withdrawRewards() external nonReentrant {
+        _updateIndex();
+
+        rewardsToken.mint(_msgSender(), rewardsAccrued[_msgSender()]);
+
+        
+        delete rewardsAccrued[_msgSender()];
+    }
+
+    function rewardsEarned(address user) external view returns (uint256) {
+        return rewardsAccrued[user] + (_getLatestRewardPerTokenIndex() - userRewardPerTokenIndex[user]) * userStakedAmount[user] / RAY;
+    }
+
+    function _updateIndex() internal {
+        if (block.timestamp > lastUpdatedTimestamp) {
+            lastRewardPerTokenIndex =  _getLatestRewardPerTokenIndex();
+
+            rewardsAccrued[_msgSender()] += (lastRewardPerTokenIndex - userRewardPerTokenIndex[_msgSender()]) * userStakedAmount[_msgSender()] / RAY;
+
+            userRewardPerTokenIndex[_msgSender()] = lastRewardPerTokenIndex;
+            lastUpdatedTimestamp = block.timestamp;
+        }
+    }
+
+    function _getLatestRewardPerTokenIndex() public view returns (uint256) {
+        if (totalStakedAmount == 0) {
+            return lastRewardPerTokenIndex;
+        } else {
+            return RAY * (block.timestamp - lastUpdatedTimestamp) * rewardEverySecond / totalStakedAmount + lastRewardPerTokenIndex;
+        }
+    }
+}
